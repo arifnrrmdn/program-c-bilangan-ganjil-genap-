3cce6e3b3c08f4754ce92e37dd7518fa8ccafb26
N4R4NT4K4 <91766087+iifrmdn@users.noreply.github.com>
2021-10-19 20:58:14 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,17 @@ int main()
   printf("Masukkan sebuah bilangan : ");
   scanf("%d", &a);
 
-  if (a % 2 == 0)
-  {
-    printf("Bilangan %d adalah genap.\n", a);
-  }
-  else
-  {
-    printf("Bilangan %d adalah ganjil.\n", a);
+  if (a < 0 ) {
+    printf("Masukan Bilangan Positif");
   }
 
+  else {
+    if (a % 2 == 0) {
+        printf("Bilangan %d adalah genap. \n", a);
+    }
+    else {
+        printf("Bilangan %d adalah ganjil. \n", a);
+    }
+  }
   return 0;
 }
